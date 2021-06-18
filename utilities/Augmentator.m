393ca8193b315541beee1b851c956aec3943e796
Andrés Morales <mora200217@hotmail.com>
2021-06-18 13:35:32 -0500

    Training Network System Added (#1)
    * added .asv
    
    * Preprocess Function Console UI"
    
    * Preprocess Function Console UI"
    
    * CleanUp
    
    * DataStore, ImageLabels, TrainedNN Added
    
    * Ignore DS_Store
    
    * Augmentator and Preprocess Added
    
    * Create Layers and YOLOv2Net class
    
    * Commented Training Notebook
    
    * Dependencies
    
    * Update README.md

---
+++ utilities/Augmentator.m
@@ -0,0 +1,23 @@
+classdef Augmentator
+    properties 
+        DataStore
+    end 
+    
+    methods
+        %% Constructor class
+        function obj = Augmentator
+            
+        end
+        
+        %% Augmentate DataStore
+        function AugmentedDatastore = Augmentate
+            DataStore
+        end
+        
+        %% Image getter
+        function getImages
+            
+        end 
+    end 
+    
+end 
